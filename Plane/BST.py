139ff563dbc0e6a76bc4db1cff0a14bc7f1d8c7e
Wanlin Li <liwanlin@mit.edu>
2020-04-21 15:22:01 -0400

    modify __str__

--- Plane/BST.py
+++ Plane/BST.py
@@ -24,40 +24,8 @@ class BSTNode:
         self.keyfunc = f
         self.height = 0
 
-    def _str(self):
-        """Internal method for ASCII art."""
-        label = self._str__()
-        if self.left is None:
-            left_lines, left_pos, left_width = [], 0, 0
-        else:
-            left_lines, left_pos, left_width = self.left._str()
-        if self.right is None:
-            right_lines, right_pos, right_width = [], 0, 0
-        else:
-            right_lines, right_pos, right_width = self.right._str()
-        middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
-        pos = left_pos + middle // 2
-        width = left_pos + middle + right_width - right_pos
-        while len(left_lines) < len(right_lines):
-            left_lines.append(' ' * left_width)
-        while len(right_lines) < len(left_lines):
-            right_lines.append(' ' * right_width)
-        if (middle - len(label)) % 2 == 1 and self.parent is not None and \
-           self is self.parent.left and len(label) < middle:
-            label += '.'
-        label = label.center(middle, '.')
-        if label[0] == '.': label = ' ' + label[1:]
-        if label[-1] == '.': label = label[:-1] + ' '
-        lines = [' ' * left_pos + label + ' ' * (right_width - right_pos),
-                 ' ' * left_pos + '/' + ' ' * (middle-2) +
-                 '\\' + ' ' * (right_width - right_pos)] + \
-          [left_line + ' ' * (width - left_width - right_width) + right_line
-           for left_line, right_line in zip(left_lines, right_lines)]
-        return lines, pos, width
-
     def __str__(self):
         return "Beachline components:\n" + self._string_() + "end"
-#        return '\n'.join(self._str()[0])
     
     def _string_(self):
         output = ""
